277749d60a36a0424ae27671e7f696dec4dfc5c9
Breedlove13 <b.breedlove@outlook.com>
2021-02-20 08:28:29 -0700

    questions + timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,77 @@
-timeIsRunning = () => {
+
+var timerEl = document.getElementById("countdown");
+var mainEl = document.getElementById("main");
+var startBtn = document.getElementById("begin");
+
+
+
+
+function quizTimer() {
   var timeLeft = 50;
 
   var timeInterval = setInterval(function () {
-    if (sec < 0) {
+    if (timeLeft === -1) {
       clearInterval(timeInterval);
-      //display meaasge 'Time has ran out'
-    } else {
-      timerEl.textcontent = `Faster, you only have: ${timeLeft} left!`;
-      time--;
+     window.confirm(
+       "You have failed miserably, and dishonored your family for generations to come"
+      //endgame()
+    }else{
+  timerEl.textContent = `Move Faster, You Have---- ${timeLeft}---- Seconds Left `;
+      timeLeft--;
     }
   }, 1000);
-};
+}
+
+startBtn.onclick = quizTimer;
+
 
 var questions = [
   {
     q: "Inside which HTML element do we put the JavaScript?",
-    choices: {
       a: "<script>",
       b: "<scripting>",
       c: "<link>",
       d: "<js>",
-    },
     answer: "a",
   },
 
   {
     q: "Which is the correct syntax for an alert?",
-    choices: {
       a: "",
       b: " hhg",
       c: "window.alert()",
       d: "haaha",
-    },
     answer: "c",
   },
 
   {
     q: "Where is the correct place to insert a JavaScript?",
-    choices: {
       a: "the <body> section",
       b: "the <head> section",
       c: "after the <html> section",
       d: " anywhere works",
-    },
     answer: "a",
   },
 
   {
     q: "How do you call a function named 'myFunction' ?",
-    choices: {
       a: "call myFunction()",
       b: " call function myFunction()",
       c: "myfunction()",
       d: "Here myFunction(), Come on boy!",
-    },
     answer: "c",
   },
 ];
+
+
+//* . End Game function for when the time runs out
+
+//endGame = () => {}
+//* create a function that when user clicks on the answer it moves to the next question,
+
+//* if right answer appears green
+//rightAnswer = () => {}
+
+//* and if they guessed wrong it turns red and deducts -10 seconds
+
+//*
