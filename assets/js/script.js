4e1f2a1b9e9e52ab51794068dde4b93fef590a48
Breedlove13 <b.breedlove@outlook.com>
2021-02-21 06:05:37 -0700

    added control flow for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,6 @@ var startBtn = document.getElementById("begin")
 var quizEl = document.getElementById("quiz")
 var questionsEl = document.getElementById("questions") 
 
-function maggotFunc() {
-  confirm("Move it Maggot");
-}
 
 
 
@@ -20,11 +17,8 @@ function quizTimer() {
      window.confirm(
        "You have failed miserably, and dishonored your family for generations to come"
       //endgame()
-        window.confirm(
-        "You have failed miserably, and dishonored your family for generations to come"
         //*  endgame()
       );
-     } else {
     }else{
   timerEl.textContent = `Move Faster, You Have---- ${timeLeft}---- Seconds Left `;
       timeLeft--;
@@ -74,23 +68,64 @@ var qQuestions = [
 ];
 
 
+//question counte
 var qCount= 0
+var correctAnswer = 0 
 
-quizEl.textContent = `You're On Question of ${Qcount + 1} of ${qQuestions.length}`;
+function viewQuestions() {
 
-console.log(quizEl.textContent)
+if(qCount >= qQuestions.length){
+
+  quizEl.textContent = "Quiz Completed"
+
+  questionsEl.textContent = `You answered ${correctAnswer} out of ${qCount} correclty`
+}
+
+
+quizEl.textContent = `You're On Question of ${qCount + 1} of ${qQuestions.length}`;
+
+
 
 questionsEl.innerHTML="<h3>"+qQuestions[qCount].q+ " </h3>";
+
+// acees question in my array
 
 chA = qQuestions[qCount].a
 chB = qQuestions[qCount].b
 chC = qQuestions[qCount].c
 chD = qQuestions[qCount].d
 
- questionsEl.innerHTML += "<label> <input type='radio' name='choices' value='A'>" +chA +"</label><br>";
+//displaying questions, had to use ""+ concat, for some reason the``$ wouldn't work
+
+  questionsEl.innerHTML += "<label> <input type='radio' name='choices' value='A'>" +chA +"</label><br>";
   questionsEl.innerHTML += "<label> <input type='radio' name='choices' value='B'>" + chB +"</label><br>";
   questionsEl.innerHTML += "<label> <input type='radio' name='choices' value='C'> " + chC + "</label><br>"
   questionsEl.innerHTML += "<label> <input type='radio' name='choices' value='D'> " + chD +"</label><br><br ";
+
+};
+function checkAnswer() {
+  //usng by name because its a looping array, selected all choices instances, i think
+  choices = document.querySelectorAll("choices");
+  for (var i = 0; i < choices.length; i++) {
+    //iterate over all 4 options
+    if (choices[i].checked) {
+      choice = choices[i].value;
+    }
+  }
+  // checks ifthe answer chosen matches the answer 
+  if (choice === qQuestions[qcount].answer) {
+   quizQuestions[qCount].answer = quizQuestions[qCount].answer.fontcolor("green") 
+
+    correct++;
+  }
+  // changes qcount of which question the user is on
+  qCount++;
+ viewQuestions();
+}
+
+
+
+window.addEventListener("load", viewQuestions);
 
 
 
@@ -105,4 +140,4 @@ chD = qQuestions[qCount].d
 
 //* and if they guessed wrong it turns red and deducts -10 seconds
 
-//*
+
