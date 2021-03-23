82fa1ee761c41ff28cb840300ce0d7a1e1488357
Breedlove13 <b.breedlove@outlook.com>
2021-03-23 08:56:29 -0700

    new simpler game plan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 
 
+//* new thought why the hell am I creating buttons just to replace them, I'll just append them into a div and wont have to waste time with the other more complicated forEach iteration, which for the lif of me I can't seem to get to work.
+
+//*POA 
+//*1. create questions
+//*list the questions using inner html and accessing them using by calling them in multi-dim array
+//* create a counter
+//*input choices using multi dim using inner html
+//*
+//*create a counter to keep the count going
+//*if/else for correct answers
+//*timer put in, connected to the correct or wrong answers
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var quest [
+  [ "What does JS stand for?", "Jahova Saint", "Javascript", "Just Sayin", "Junior Salamandor", "Javsacript" ],
+  ["Where "],
+  [],
+  [],
+]
